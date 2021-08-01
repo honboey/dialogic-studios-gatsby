0e0110ab89199413734a97736c95650637475c93
Hon Boey <hon@dialogicstudios.com.au>
2021-08-02 09:24:35 +1000

    Mapped over SingleLandscapeImage to allow multiple images in single slice

--- src/components/SingleLandscapeImageSlice.js
+++ src/components/SingleLandscapeImageSlice.js
@@ -1,11 +1,17 @@
 import * as React from "react";
 import "../styles/global.css";
-import { getImage, GatsbyImage } from "gatsby-plugin-image";
+import { GatsbyImage } from "gatsby-plugin-image";
 
 function SingleLandscapeImageSlice(props) {
     console.log(props)
     return (
-        <GatsbyImage className="w-full mb-6 md:mb-10 xl:mb-14" image={props.slice.items[0].landscape_image.gatsbyImageData} alt="Two boys sitting on a porch in rural Australia" />
+        <>
+            {props.slice.items.map((image, index) => (
+                <GatsbyImage 
+                    className="w-full mb-6 md:mb-10 xl:mb-14"
+                    image={image.landscape_image.gatsbyImageData} />
+            ))}    
+        </>
     )
 }
 
