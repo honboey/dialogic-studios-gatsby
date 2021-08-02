83e4f75ebfeb1d34873dd770ed3120d90bd35f8a
Hon Boey <hon@dialogicstudios.com.au>
2021-08-02 23:53:50 +1000

    WIP work filter.

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -5,13 +5,13 @@ import "../styles/global.css";
 
 function ProjectItem(props) {
     return (
-        <li className="xl:flex mb-8 md:mb-12 xl:mb-16 transition ease-in duration-200 all art-direction editorial campaign" >
+        <li className={"xl:flex mb-8 md:mb-12 xl:mb-16 transition ease-in duration-200" + props.display + props.opacity} >
             <div className="xl:w-2/5">
             <h2 className="text-lg sm:text-2xl lg:text-3xl xl:text-4xl">{props.projectName}</h2>
             <p className="text-sm sm:text-lg lg:text-xl xl:text-2xl text-ds-grey mb-1 md:mb-3 xl:mb-4" >
                 {/* List services and capitalise the first letter of each item */}
                 {props.services.map((service, index) => {
-                    if (index < props.services.length - 1) {
+                    if (index < props.services.length - 1) { 
                         return service.charAt(0).toUpperCase() + service.slice(1) + ", "
                     }
                     return service.charAt(0).toUpperCase() + service.slice(1)                    
--- src/pages/work.js
+++ src/pages/work.js
@@ -6,11 +6,45 @@ import { getImage } from "gatsby-plugin-image";
 import "../styles/global.css";
 
 function WorkPage( {data} ) {
-  const [displayArray, filterDisplayArray] = useState(data)
-  console.log(displayArray)
+  const [displayArray, setDisplayArray] = useState(data.allPrismicProject.nodes)
+  const [display, setDisplay] = useState(" display-block")
+  const [opacity, setOpacity] = useState(" opacity-100")
+  const [items, setItems] = useState([1, 2, 3, 4, 5, 6])
 
+  function filterItems() {
+    setDisplayArray(displayArray => displayArray.filter(element => element.uid=="nla"))
+  }
+
+  function filterClickHandler() {
+   setTimeout(
+     function() {
+       filterItems()
+      }, 
+      3000)
+  }
+
+  function fadeOutClickHandler() {
+    setTimeout(setOpacity(" opacity-0"), 2000)
+  }
+
+  function toggleDisplay() {
+    setDisplay(" hidden")
+  }
+
+  function displayClickHandler() {
+    setTimeout(function() {
+      setDisplay(" hidden")
+    }, 1000)
+  }
+
+
+  
+  
   return (
     <Layout>
+      <p>{items}</p>
+      <button onClick={fadeOutClickHandler} className="text-red-500">Test</button>
+      <button onClick={displayClickHandler}>Display</button>
       <div className="xl:flex">
         <div className="flex xl:block xl:w-1/6 text-sm sm:text-lg lg:text-xl xl:text-2xl leading-normal lg:leading-normal xl:leading-normal border-t border-b border-black xl:border-none py-3 mb-8 md:mb-12 xl:mb-16">
           <div className="w-1/2 md:w-5/12 lg:w-1/2 xl:w-full mb-4 lg:mb-8">
@@ -19,7 +53,10 @@ function WorkPage( {data} ) {
           <div id="services" className="w-1/2 md:w-7/12 lg:w-1/2 xl:w-full flex xl:block" >
             <div className="w-1/2 xl:w-full">
               <ul>
-                <li><p className="all text-ds-orange">All</p></li>
+                <li><button 
+                  className="all text-ds-orange"
+                  onClick={filterClickHandler}
+                  >All</button></li>
                 <li><p className="identity">Identity</p></li>
                 <li><p className="art-direction">Art direction</p></li>
                 <li><p className="web">Web</p></li>
@@ -37,17 +74,18 @@ function WorkPage( {data} ) {
 
         <section id="work" className="xl:w-5/6">
           <ul>
-            { 
-              data.allPrismicProject.nodes.map((element) => (
-                <ProjectItem
-                  key={element.uid} 
-                  projectName={element.data.project_title.text}
-                  services={element.tags}
-                  featureImage={getImage(element.data.feature_image.gatsbyImageData)}
-                  altText={element.data.feature_image.alt}
-                  slug={element.uid}
-                  />
-              ))
+            {displayArray.map((element) => (
+              <ProjectItem
+                key={element.uid} 
+                projectName={element.data.project_title.text}
+                services={element.tags}
+                featureImage={getImage(element.data.feature_image.gatsbyImageData)}
+                altText={element.data.feature_image.alt}
+                slug={element.uid}
+                opacity={opacity}
+                display={display}
+                />
+            ))
             }
           </ul>
         </section>
