015c49b6fbafe8cb8ab5ce7e838d90bc5815a1f6
Hon Boey <hon@dialogicstudios.com.au>
2021-08-04 15:52:31 +1000

    Updated gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ require("dotenv").config({
 
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
+    siteUrl: "https://www.dialogicstudios.com.au",
     title: "Dialogic Studios",
   },
   plugins: [
