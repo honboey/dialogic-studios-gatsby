a7bf2f2386280555dd5f5e00da05561800bfe015
Hon <55781260+honboey@users.noreply.github.com>
2024-03-22 12:34:57 +1100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ function IndexPage( {data} ) {
   return (
     <Layout>
       <section className="text-2xl leading-normal lg:text-3xl lg:leading-normal xl:text-4xl xl:leading-normal md:w-7/12 lg:w-1/2 md:float-right mb-8 md:mb-12 xl:mb-16">
-        <p>Dialogic Studios is the personal graphic design practice of Hon Boey. We work on Gadigal land in Sydney, Australia.</p>
+        <p>Dialogic Studios is the personal graphic design practice of Hon Boey. I work on Gadigal land in Sydney, Australia.</p>
       </section>
 
       <section className="clear-right mb-8 md:mb-12 xl:mb-16">
@@ -37,7 +37,7 @@ function IndexPage( {data} ) {
           <h2 className="text-lg sm:text-2xl lg:text-3xl xl:text-4xl">Contact us</h2>
         </div>
         <div className="w-2/3 md:w-1/2">
-          <p className="mb-4 lg:mb-8">If you have a project that you’d like to collaborate on then please drop us a line. We’d love to hear from you.</p>
+          <p className="mb-4 lg:mb-8">I'm currently not taking any commercial projects but if you'd like to collaborate on a personal work then I’d love to hear from you.</p>
 
           <p className="mb-4 lg:mb-8">
             <a href="mailto:hello@dialogicstudios.com.au">hello@dialogicstudios.com.au</a><br/>
