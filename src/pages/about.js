bb00fe7c448d5f5e9d1c99dd1f97e5d18d03c305
Hon <55781260+honboey@users.noreply.github.com>
2024-03-25 19:57:01 +1100

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -23,7 +23,7 @@ function AboutPage() {
         </div>
         <figcaption className="md:order-1 mb-8 lg:mb-16 text-xs sm:text-sm lg:base">Photo by Nisha Agiasotis</figcaption>
         <div className="text-2xl leading-normal lg:text-3xl lg:leading-normal xl:text-4xl xl:leading-normal md:w-7/12 lg:w-1/2 flex-shrink-0 md:order-3">
-          <p className="mb-8 lg:mb-16">Since 2017, I've been lucky to work with clients such as <Link to="/stc">Sydney Theatre Company</Link>, <Link to="/nla">the National Library of Australia</Link>, <Link to="/unsw-merch">UNSW</Link> and <Link to="/rise"> Refugees, Inmates, Survivors and Ex-detainees (RISE)</Link>.</p>
+          <p className="mb-8 lg:mb-16">Since 2017, I've been lucky to work with clients such as <Link to="/projects/stc">Sydney Theatre Company</Link>, <Link to="/projects/nla">the National Library of Australia</Link>, <Link to="/projects/unsw-merch">UNSW</Link> and <Link to="/projects/rise"> Refugees, Inmates, Survivors and Ex-detainees (RISE)</Link>.</p>
           <p className="mb-8 lg:mb-16">I am currently a principal at <a href="https://interaction.net.au/">Interaction Consortium</a>.</p>
           <p className="mb-4 lg:mb-8">In my spare time I <a href="https://www.sydney.edu.au/courses/units-of-study/2020/cael/cael3018.html"> teach design at Sydney University</a>, run the <a href="https://www.otherworldszines.com/"> Other Worlds Zine Fair</a>, look after a <a href="http://www.psychichysteria.com/">record & publishing label</a> and take photos of <a href="http://www.psychichysteria.com/shop/clothes-bros"> people wearing the same clothes</a>.</p>
         </div>
